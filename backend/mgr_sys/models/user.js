4c30c549172541ff57f2e3ff99ba88f27ac77604
yhlyj <lyhdn@qq.com>
2019-01-16 16:13:33 +0800

    del backend

--- backend/mgr_sys/models/user.js
+++
@@ -1,6 +0,0 @@
-const db = require('')
-const tableName = 'users'
-module.exports = db.defineModel(tableName, {
-    name: db.STRING(100),
-    passwd: db.STRING(100)
-});
--- backend/mgr_sys/models/user_house_info.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db');
-
-module.exports = db.defineModel('user_house_info', {
-    userName: db.STRING(100),
-    houseName: db.STRING(100),
-    houseLayerNum: db.INTEGER,
-    houseRoomNum: db.INTEGER
-});
